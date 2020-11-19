f564fe0ea02ade053cba0a213c530b0ff8c3e894
L. Gehring <l.gehring@student.uni-tuebingen.de>
2020-11-19 19:15:51 +0100

    Added scrapy spider to fetch data from one url Added possibility to call spider directly from script

--- teamproject/crawler.py
+++ teamproject/crawler.py
@@ -1,14 +1,18 @@
 """
 This module contains code to fetch required data from the internet and convert
-it to our internal format.
+it to a pd.DataFrame.
 """
 
 import pandas as pd
+import scrapy
+import json
+
+from scrapy.crawler import CrawlerProcess
 
 
 def fetch_data():
     """
-    Query data from "the internet" and return in our internal format.
+    Just for testing. Specifies required file format
     """
     # For now just return some example data in some example format:
     columns = ['home_team', 'home_score', 'guest_score', 'guest_team']
@@ -18,3 +22,36 @@ def fetch_data():
         ['Tübingen', 1, 0, 'Leverkusen'],
         ['Bremen', 0, 1, 'Leverkusen'],
     ], columns=columns)
+
+
+# TODO: add possibility to choose what data should be scraped given by gui.py
+# TODO: adjust scraped data to required format defined above
+def fetch_data_new():
+    """
+    Query data from "the internet" and return as pd.DataFrame.
+    """
+    process = CrawlerProcess()
+    process.crawl(OpenLigaSpider)
+    process.start()
+
+
+class OpenLigaSpider(scrapy.Spider):
+    """
+        Query sample match and print full json-string
+    """
+    name = "OpenLigaSpider"
+
+    def start_requests(self):
+        # Example: Spiele des 1. Spieltages der ersten Bundesliga 2020/2021
+        urls = ['https://api.openligadb.de/getmatchdata/bl1/2020/1']
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    # suppresses false warning: can be ignored
+    # noinspection PyMethodOverriding
+    def parse(self, response):
+        jsonresponse = json.loads(response.body)
+        print(jsonresponse)  # all available data
+
+# To scrape data call uncomment fetch_data_new()
+# fetch_data_new()
