2d64de56c70a6bc3b33cdff0c48e72eca9756a5f
FabricioAguilera <fabricio.aguilera-galviz@student.uni-tuebingen.de>
2020-11-23 21:12:54 +0100

    implementing gui prototype 01

--- teamproject/gui.py
+++ teamproject/gui.py
@@ -1,6 +1,11 @@
 """
 Add your GUI code here.
 """
+# Importing tkinter module
+import sqlite3
+from tkinter import *
+
+import data as data
 
 from teamproject.crawler import fetch_data
 from teamproject.models import FrequencyModel
@@ -12,9 +17,54 @@ def main():
     """
     # Add code here to create and initialize window.
 
-    # For demo purposes, this is how you could access methods from other
-    # modules:
-    data = fetch_data()
-    model = FrequencyModel(data)
-    winner = model.predict_winner('Tübingen', 'Leverkusen')
-    print(winner)
+
+# creating Tk window
+root = Tk()
+root.title("Football League Matches Predictor")
+root.geometry("400x400")
+'''
+mainMenu = Menu(root)
+root.config(menu=mainMenu)
+
+homeTeamMenu = Menu(mainMenu)
+mainMenu.add_cascade(label="Home Team", menu=homeTeamMenu)
+#subMenu.add.command...
+
+
+awayTeamMenu = Menu(mainMenu)
+mainMenu.add_cascade(label="Away Team", menu=awayTeamMenu)
+
+'''
+#create a database or connect to one
+#conn =sqlite3.connect(data)
+
+#create cursor
+#c = conn.cursor()
+
+#create table
+#c.exe
+
+def crawlerClick():
+    crawlerData = Label(root, data)
+    crawlerData.pack()
+
+
+actCrawlerButton = Button(root, text="Activate Crawler!", command=crawlerClick)
+actCrawlerButton.pack()
+trainMLButton = Button(root, text="Start the Algorithm!")
+trainMLButton.pack()
+winProbButton = Button(root, text="Show win probability percent!")
+winProbButton.pack()
+
+#drop down lists for teams
+
+#def listHomeTeam()
+
+
+root.mainloop()
+# For demo purposes, this is how you could access methods from other
+# modules:
+data = fetch_data()
+model = FrequencyModel(data)
+winner = model.predict_winner('Tübingen', 'Leverkusen')
+print(winner)
