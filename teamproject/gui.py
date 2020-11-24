0d4e525b3511583e5a7f20e69c4bb5137dea654e
“astammer” <anabel.stammer@student.uni-tuebingen.de>
2020-11-24 14:09:33 +0100

    changed the options of drop down lists

--- teamproject/gui.py
+++ teamproject/gui.py
@@ -3,6 +3,7 @@ Add your GUI code here.
 """
 # Importing tkinter module
 from tkinter import *
+import pandas as pd
 
 import data as data
 
@@ -21,8 +22,6 @@ def main():
 root = Tk()
 root.title("Football League Matches Predictor")
 root.geometry("400x400")
-selected_guest_team = ''
-selected_home_team = ''
 
 
 def fetch_crawler_data():
@@ -35,44 +34,46 @@ def train_model():
     return trained_model
 
 
-def pick_guestteam(*args):
-    selected_guest_team.var_guest_team.get()
+def pick_guestteam(list, *args):
+    picked_guest_team = var_home_team.get()
 
 
-def pick_hometeam(*args):
-    selected_home_team.var_home_team.get()
+def pick_hometeam(list, *args):
+    picked_home_team = var_home_team.get()
 
 
 actCrawlerButton = Button(root, text="Activate Crawler!", command=fetch_crawler_data)
 actCrawlerButton.pack()
 trainMLButton = Button(root, text="Start the Algorithm!", command=train_model)
 trainMLButton.pack()
-#winProbButton = Button(root, text="Show win probability percent!",
- #                      command=FrequencyModel.predict_winner(train_model(), selected_home_team, selected_guest_team))
-#winProbButton.pack()
+trained_model = train_model()
 
-# def listHomeTeam()
+# winProbButton = Button(root, text="Show win probability percent!",
+#                      command=trained_model.predict_winner(picked_teams[0], picked_teams[1]))
+# winProbButton.pack()
+
+
 # drop down lists for teams
-OptionList_home_team = fetch_crawler_data()['home_team']
+# make option list
+OptionList = fetch_crawler_data()['home_team']
+#append list of hometeams with list of guestteam
+OptionList = OptionList.append(fetch_crawler_data()['guest_team'])
+OptionList = OptionList.drop_duplicates()
+
+#make hometeam dropdown
 var_home_team = StringVar(root)
-var_home_team.set(OptionList_home_team[0])
-opt_ht = OptionMenu(root, var_home_team, *OptionList_home_team)
+var_home_team.set(OptionList[0])
+opt_ht = OptionMenu(root, var_home_team, *OptionList)
 opt_ht.config(width=10, font=('Helvetica', 12))
 var_home_team.trace("w", pick_hometeam)
 opt_ht.pack()
 
-OptionList_guest_team = fetch_crawler_data()['home_team']
+#make guestteam dropdown
 var_guest_team = StringVar(root)
-var_guest_team.set(OptionList_guest_team[0])
-opt_gt = OptionMenu(root, var_guest_team, *OptionList_guest_team)
+var_guest_team.set(OptionList[0])
+opt_gt = OptionMenu(root, var_guest_team, *OptionList)
 opt_gt.config(width=10, font=('Helvetica', 12))
 var_guest_team.trace("w", pick_guestteam)
 opt_gt.pack()
 
 root.mainloop()
-# For demo purposes, this is how you could access methods from other
-# modules:
-data = fetch_data()  # TODO: unnecessary : remove when implemented
-model = FrequencyModel()
-winner = model.predict_winner('Tübingen', 'Leverkusen')
-print(winner)
