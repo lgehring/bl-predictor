6278a1287ca5c8867dc0f5860fe7cf0d7495836f
L. Gehring <l.gehring@student.uni-tuebingen.de>
2020-11-24 19:30:57 +0100

    Added WholeDataFrequencies class in models.py that calculates basic statistics about the dataset for prediction comparison Added appropriate tests to test_models.py Tested the new class

--- tests/test_models.py
+++ tests/test_models.py
@@ -41,10 +41,11 @@ missing_column = pd.DataFrame([
     ['A', 0, 1],
     ['B', 0, 0],
     ['A', 3, 1],
-], columns=['home_team', 'home_score', 'guest_score'])
+], columns=['home_team', 'home_score', 'guest_team'])
 
 
-def test_norm():
+# FrequencyModel testset
+def test_frequency_model_norm():
     model = models.FrequencyModel(norm_train)
     winner = model.predict_winner
     assert winner('A', 'B') == winner('B', 'A') == 'B'
@@ -54,7 +55,7 @@ def test_norm():
     assert winner('A', 'E') == winner('E', 'A') is None
 
 
-def test_nonsense():
+def test_frequency_model_nonsense():
     model = models.FrequencyModel(nonsense_matches)
     winner = model.predict_winner
     assert winner('A', 'B') == winner('B', 'A') == 'A'
@@ -62,7 +63,7 @@ def test_nonsense():
     assert winner('B', 'C') == winner('C', 'B') == 'C'
 
 
-def test_empty():
+def test_frequency_model_empty():
     model = models.FrequencyModel(empty_data)
     winner = model.predict_winner
     assert winner('A', 'B') == winner('B', 'A') is None
@@ -70,13 +71,13 @@ def test_empty():
     assert winner('B', 'C') == winner('C', 'B') is None
 
 
-def test_no_matchups():
+def test_frequency_model_no_matchups():
     model = models.FrequencyModel(no_matchups)
     winner = model.predict_winner
     assert winner('A', 'B') == winner('B', 'A') is None
 
 
-def test_too_many_columns():
+def test_frequency_model_too_many_columns():
     model = models.FrequencyModel(too_many_columns)
     winner = model.predict_winner
     assert winner('A', 'B') == winner('B', 'A') == 'B'
@@ -84,9 +85,64 @@ def test_too_many_columns():
     assert winner('B', 'C') == winner('C', 'B') == 'C'
 
 
-def test_missing_column():
+def test_frequency_model_missing_column():
     model = models.FrequencyModel(missing_column)
     winner = model.predict_winner
     assert winner('A', 'B') == winner('B', 'A') is None
     assert winner('C', 'A') == winner('A', 'C') is None
     assert winner('B', 'C') == winner('C', 'B') is None
+
+
+# WholeDataFrequencies testset
+def test_whole_data_frequencies_norm_train():
+    model = models.WholeDataFrequencies(norm_train)
+    assert model.home_team_wins == 1
+    assert model.guest_team_wins == 2
+    assert model.draws == 0
+    assert model.home_team_avg_goals == 4 / 3
+    assert model.guest_team_avg_goals == 4 / 3
+
+
+def test_whole_data_frequencies_nonsense():
+    model = models.WholeDataFrequencies(nonsense_matches)
+    assert model.home_team_wins == 2
+    assert model.guest_team_wins == 1
+    assert model.draws == 0
+    assert model.home_team_avg_goals == 4 / 3
+    assert model.guest_team_avg_goals == 333
+
+
+def test_whole_data_frequencies_empty():
+    model = models.WholeDataFrequencies(empty_data)
+    assert model.home_team_wins == 0
+    assert model.guest_team_wins == 0
+    assert model.draws == 0
+    assert model.home_team_avg_goals is None
+    assert model.guest_team_avg_goals is None
+
+
+def test_whole_data_frequencies_no_matchups():
+    model = models.WholeDataFrequencies(no_matchups)
+    assert model.home_team_wins == 0
+    assert model.guest_team_wins == 2
+    assert model.draws == 1
+    assert model.home_team_avg_goals == 1/3
+    assert model.guest_team_avg_goals == 7 / 3
+
+
+def test_whole_data_frequencies_too_many_columns():
+    model = models.WholeDataFrequencies(too_many_columns)
+    assert model.home_team_wins == 1
+    assert model.guest_team_wins == 2
+    assert model.draws == 0
+    assert model.home_team_avg_goals == 1
+    assert model.guest_team_avg_goals == 2
+
+
+def test_whole_data_frequencies_missing_column():
+    model = models.WholeDataFrequencies(missing_column)
+    assert model.home_team_wins == 0
+    assert model.guest_team_wins == 0
+    assert model.draws == 0
+    assert model.home_team_avg_goals is None
+    assert model.guest_team_avg_goals is None
