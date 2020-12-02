d26b487442f9535264d210e2d50854deee4c769a
L. Gehring <l.gehring@student.uni-tuebingen.de>
2020-12-03 00:28:53 +0100

    Adjusted test_crawler.py to get rid of unnecessary tests

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -3,9 +3,10 @@
 from teamproject import crawler
 import unittest
 
+
 class test_crawler(unittest.TestCase):
     # Example test:
-    '''
+    """
     def test_fetch_data():
         data = crawler.fetch_data()
         assert isinstance(data, pd.DataFrame)
@@ -14,17 +15,4 @@ class test_crawler(unittest.TestCase):
         assert (data.home_score >= 0).all()
         assert (data.guest_score >= 0).all()
         assert (data.home_team != data.guest_team).all()
-    '''
-
-
-    def test_get_all_urls(self):
-        assert (crawler.get_all_urls(1, 2014, 2, 2014)) == [ \
-            'https://api.openligadb.de/getmatchdata/bl1/2014/1', \
-            'https://api.openligadb.de/getmatchdata/bl1/2014/2']
-        assert (crawler.get_all_urls(1, 2014, 1, 2014)) == \
-        ['https://api.openligadb.de/getmatchdata/bl1/2014/1']
-        assert (crawler.get_all_urls(1, 2014, 8, 2016))[-1] == \
-            "https://api.openligadb.de/getmatchdata/bl1/2016/8"
-        assert (crawler.get_all_urls(1, 2014, 8, 2016))[0] == \
-            "https://api.openligadb.de/getmatchdata/bl1/2014/1"
-        self.assertRaises(ValueError, lambda: crawler.get_all_urls(0, 2014, 8, 2016))
+    """
