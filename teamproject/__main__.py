29fc4f4ce9fd426ebb449f4ce530bee418c93817
L. Gehring <l.gehring@student.uni-tuebingen.de>
2020-12-08 11:52:46 +0100

    fixed typo

--- teamproject/__main__.py
+++ teamproject/__main__.py
@@ -4,4 +4,4 @@ Run the application.
 This module is invoked when calling ``python -m teamproject``.
 """
 from teamproject import gui
-gui.main_window().show_window()
+gui.MainWindow().show_window()
