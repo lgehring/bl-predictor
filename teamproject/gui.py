1dbc09be2e4651a1b95db61b0044af78d85f3327
astammer <anabel.stammer@student.uni-tuebingen.de>
2021-01-22 14:48:25 +0100

    flake8

--- teamproject/gui.py
+++ teamproject/gui.py
@@ -107,7 +107,6 @@ class MainWindow:
             self.panel1.photo = self.img1
             self.panel2.photo = self.img2
 
-
             # shows date and time of each match
             day_label = tk.Label(text=matchday['date_time'][i])
             day_label.pack()
