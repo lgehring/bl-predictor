09c674bd7be04f4b1a3281ad605df2b8fa31b522
stuggialex <63969996+stuggialex@users.noreply.github.com>
2021-01-24 18:37:06 +0100

    Merge gui_upgrade into master (#38)
    * restored gui changes
    
    * merge
    
    * logos get pulled from api if they are not in "Logos"
    
    * added comments to the new function save_logos
    
    * changed the size of the shown logos
    
    * removed brackets
    
    * deleted berlin logo
    
    * predictions now show correctly
    
    Co-authored-by: astammer <anabel.stammer@student.uni-tuebingen.de>
    Co-authored-by: L. Gehring <l.gehring@student.uni-tuebingen.de>
    Co-authored-by: L. Gehring <44134586+lgehring@users.noreply.github.com>

--- teamproject/crawler.py
+++ teamproject/crawler.py
@@ -4,6 +4,7 @@ it to a pd.DataFrame.
 """
 import datetime
 import json
+import os
 
 import pandas as pd
 import requests
@@ -174,6 +175,12 @@ def crawl_openligadb(urls):
             return True
 
         for game in range(len(jsonresponse)):  # all matches in scrape
+
+            save_logos(jsonresponse[game]['team1']['teamName'],
+                       jsonresponse[game]['team1']['teamIconUrl'])
+            save_logos(jsonresponse[game]['team2']['teamName'],
+                       jsonresponse[game]['team2']['teamIconUrl'])
+
             # appends response item-array to matches, !ORDER SENSITIVE!
             if jsonresponse[game]['matchIsFinished']:
                 matches_length = len(matches)
@@ -195,3 +202,19 @@ def crawl_openligadb(urls):
                     -1,  # g
                     jsonresponse[game]['team2']['teamName']  # guest_t]
                 ]
+
+
+def save_logos(teamname, teamicon):
+    """
+    checks if logos are missing in the folder "Logos" and download,
+    if necessary
+
+    :param teamname: string with the name the file should be called
+    :param teamname: Web URL where the image is saved
+    """
+    gui_path = os.path.abspath(__file__)
+    dir_path = os.path.dirname(gui_path)
+    if not os.path.isfile(dir_path + "/Logos/" + teamname + ".png"):
+        save_logo = open(dir_path + "/Logos/" + teamname + ".png", "wb")
+        save_logo.write(requests.get(teamicon).content)
+        save_logo.close()
